c48c6395ccf6c8e4b9120c57de0b4613084ce1fc
virzarasydann <virzarasydan@gmail.com>
2024-07-27 09:27:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,15 +17,15 @@ const sequelize = require('./database/index'); //import sequelize models
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
